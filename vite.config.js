e6b762408b3845b25ee8db92c07dff0add1e52be
Dhruvesh-Gangapuram <gangapuramdhruvesh@gmail.com>
2026-07-15 17:25:47 +0530

    Configure GitHub Pages deployment

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // If your GitHub repository is https://github.com/username/resume-website,
+  // change the base path to '/resume-website/'. 
+  // If it's your main user site (username.github.io), set it to '/'.
+  base: '/resume-website/',
+});
